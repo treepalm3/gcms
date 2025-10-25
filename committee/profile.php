a5d30815cd2562155f9b5ae2943eef71ca65a6fe
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 02:01:35 +0700

    ลบไฟล์รายงาน

--- committee/profile.php
+++ committee/profile.php
@@ -243,10 +243,8 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
       <div class="side-brand mb-2"><h3><span>Committee</span></h3></div>
       <nav class="sidebar-menu">
         <a href="committee_dashboard.php"><i class="fa-solid fa-border-all"></i>ภาพรวม</a>
-        <a href="report.php"><i class="fa-solid fa-chart-line"></i>รายงาน</a>
         <a href="finance.php"><i class="fa-solid fa-wallet"></i> การเงินและบัญชี</a>
         <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
-        <a href="points.php"><i class="fa-solid fa-star"></i> คะแนนสะสม</a>
         <a href="member.php"><i class="bi bi-people-fill"></i> สมาชิก</a>
         <a href="profile.php" class="active"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
       </nav>
@@ -260,10 +258,8 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
         <div class="side-brand mb-3"><h3><span>Committee</span></h3></div>
         <nav class="sidebar-menu flex-grow-1">
           <a href="committee_dashboard.php"><i class="fa-solid fa-border-all"></i>ภาพรวม</a>
-          <a href="report.php"><i class="fa-solid fa-chart-line"></i>รายงาน</a>
           <a href="finance.php"><i class="fa-solid fa-wallet"></i> การเงินและบัญชี</a>
           <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
-          <a href="points.php"><i class="fa-solid fa-star"></i> คะแนนสะสม</a>
           <a href="member.php"><i class="bi bi-people-fill"></i> สมาชิก</a>
           <a href="profile.php" class="active"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
         </nav>
