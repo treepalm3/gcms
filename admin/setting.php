1ff5d0b98cf3e085dc9462f5f7065a19c120f965
treepalm3 <palmmy.tree13@gmail.com>
2025-10-25 20:13:33 +0700

    ลบส่วนรายงาน

--- admin/setting.php
+++ admin/setting.php
@@ -261,7 +261,6 @@ $avatar_text = mb_substr($current_name, 0, 1, 'UTF-8');
       <a href="member.php"><i class="bi bi-people-fill"></i> สมาชิก</a>
       <a href="finance.php"><i class="bi bi-wallet2"></i> การเงินและบัญชี</a>
       <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
-      <a href="report.php"><i class="fa-solid fa-chart-line"></i>รายงาน</a>
       <a class="active" href="setting.php"><i class="bi bi-gear-fill"></i> ตั้งค่าระบบ</a>
     </nav>
     <a class="logout mt-auto" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
@@ -282,7 +281,6 @@ $avatar_text = mb_substr($current_name, 0, 1, 'UTF-8');
         <a href="member.php"><i class="bi bi-people-fill"></i> สมาชิก</a>
         <a href="finance.php"><i class="fa-solid fa-wallet"></i> การเงินและบัญชี</a>
         <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
-        <a href="report.php"><i class="fa-solid fa-chart-line"></i> รายงาน</a>
         <a class="active" href="setting.php"><i class="bi bi-gear-fill"></i> ตั้งค่า</a>
       </nav>
       <a class="logout" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
