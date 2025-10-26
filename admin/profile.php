ac2d2fb4be9d514c2ffc3b1183fada56c483c290
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 13:31:16 +0700

    แก้ออกจากระบบ

--- admin/profile.php
+++ admin/profile.php
@@ -343,7 +343,7 @@ $avatar_text = mb_substr($current_name, 0, 1, 'UTF-8');
         <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
         <a href="setting.php"><i class="bi bi-gear-fill"></i> ตั้งค่า</a>
       </nav>
-      <a class="logout mt-auto" href="../index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+      <a class="logout mt-auto" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
     </div>
   </div>
 
@@ -363,7 +363,7 @@ $avatar_text = mb_substr($current_name, 0, 1, 'UTF-8');
           <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
           <a href="setting.php"><i class="bi bi-gear-fill"></i> ตั้งค่า</a>
         </nav>
-        <a class="logout" href="../index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+        <a class="logout" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
       </div>
 
       <!-- Content -->
