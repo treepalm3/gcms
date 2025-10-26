49528a08c3d392267f438c4741b030177ccfe591
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 15:07:06 +0700

    แก้ออกจากระบบ

--- member/dividend.php
+++ member/dividend.php
@@ -274,7 +274,7 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
         <a class="active" href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล & เฉลี่ยคืน</a>
         <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
       </nav>
-      <a class="logout mt-auto" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+      <a class="logout mt-auto" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
     </div>
   </div>
 
@@ -289,7 +289,7 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
           <a class="active" href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล & เฉลี่ยคืน</a>
           <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
         </nav>
-        <a class="logout" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+        <a class="logout" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
       </aside>
 
       <main class="col-lg-10 p-4">
