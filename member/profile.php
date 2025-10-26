57784af5464ed447936b1f92c0f2bdae7a8e2015
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 15:08:37 +0700

    แก้ออกจากระบบ

--- member/profile.php
+++ member/profile.php
@@ -272,7 +272,7 @@ $avatar_text = mb_substr($current_name, 0, 1, 'UTF-8');
         <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
         <a class="active" href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
       </nav>
-      <a class="logout mt-auto" href="../index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+      <a class="logout mt-auto" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
     </div>
   </div>
 
@@ -288,7 +288,7 @@ $avatar_text = mb_substr($current_name, 0, 1, 'UTF-8');
           <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
           <a class="active" href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
         </nav>
-        <a class="logout" href="../index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+        <a class="logout" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
       </aside>
 
       <!-- Content -->
