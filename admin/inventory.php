81f0a67dd50ebab14af5811c2ad169455fc798e9
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 13:29:32 +0700

    ปรับออกจากระบบ

--- admin/inventory.php
+++ admin/inventory.php
@@ -240,7 +240,7 @@ $utilization    = $total_capacity > 0 ? ($total_stock / $total_capacity) * 100 :
             <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
             <a href="setting.php"><i class="bi bi-gear-fill"></i> ตั้งค่า</a>
         </nav>
-        <a class="logout mt-auto" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+        <a class="logout mt-auto" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
     </div>
 </div>
 
@@ -259,7 +259,7 @@ $utilization    = $total_capacity > 0 ? ($total_stock / $total_capacity) * 100 :
                 <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
                 <a href="setting.php"><i class="bi bi-gear-fill"></i> ตั้งค่า</a>
             </nav>
-            <a class="logout" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+            <a class="logout" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
         </aside>
 
         <main class="col-lg-10 p-4">
