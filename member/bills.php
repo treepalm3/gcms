3f1896f51e003a4bf0782a86550ce24a80223299
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 15:05:53 +0700

    แก้ออกจากระบบ

--- member/bills.php
+++ member/bills.php
@@ -289,7 +289,7 @@ sort($pay_methods);
         <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
         <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
       </nav>
-      <a class="logout mt-auto" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+      <a class="logout mt-auto" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
     </div>
   </div>
 
@@ -304,7 +304,7 @@ sort($pay_methods);
           <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
           <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
         </nav>
-        <a class="logout" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+        <a class="logout" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
       </aside>
 
       <main class="col-lg-10 p-4">
