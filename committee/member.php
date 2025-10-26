4ffeae3465ae8bdcba2b88e8b94f861c5f4b73b9
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 14:40:24 +0700

    แก้ออกจากระบบ

--- committee/member.php
+++ committee/member.php
@@ -213,7 +213,7 @@ foreach ($members as $m) {
       <a href="member.php" class="active"><i class="bi bi-people-fill"></i> สมาชิก</a>
       <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
     </nav>
-    <a class="logout mt-auto" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+    <a class="logout mt-auto" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
   </div>
 </div>
 
@@ -229,7 +229,7 @@ foreach ($members as $m) {
         <a href="member.php" class="active"><i class="bi bi-people-fill"></i> สมาชิก</a>
         <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
       </nav>
-      <a class="logout" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
+      <a class="logout" href="../index.php"><i class="fa-solid fa-right-from-bracket"></i>ออกจากระบบ</a>
     </aside>
 
     <!-- Main -->
