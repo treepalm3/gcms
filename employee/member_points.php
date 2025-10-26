920a53aa4cc590473abebd5037af4d3e5978404e
treepalm3 <palmmy.tree13@gmail.com>
2025-10-26 15:00:37 +0700

    แก้ออกจากระบบ

--- employee/member_points.php
+++ employee/member_points.php
@@ -275,7 +275,7 @@ if ($db_ok && $search_term !== '') {
         <a class="active" href="member_points.php"><i class="bi bi-star-fill"></i> แต้มสมาชิก</a>
         <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
       </nav>
-      <a class="logout mt-auto" href="/index/logout.php"><i class="bi bi-box-arrow-right"></i>ออกจากระบบ</a>
+      <a class="logout mt-auto" href="../index.php"><i class="bi bi-box-arrow-right"></i>ออกจากระบบ</a>
     </div>
   </div>
 
@@ -291,7 +291,7 @@ if ($db_ok && $search_term !== '') {
           <a class="active" href="member_points.php"><i class="bi bi-star-fill"></i> แต้มสมาชิก</a>
           <a href="profile.php"><i class="fa-solid fa-user-gear"></i> โปรไฟล์</a>
         </nav>
-        <a class="logout" href="/index/logout.php"><i class="fa-solid fa-right-from-bracket me-1"></i>ออกจากระบบ</a>
+        <a class="logout" href="../index.php"><i class="fa-solid fa-right-from-bracket me-1"></i>ออกจากระบบ</a>
       </aside>
 
       <main class="col-lg-10 p-4">
