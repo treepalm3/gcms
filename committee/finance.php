e4033a1f3361fb99365a19b039613d55498dd67f
treepalm3 <palmmy.tree13@gmail.com>
2025-10-28 07:59:11 +0700

    แก้แดชบอร์ด

--- committee/finance.php
+++ committee/finance.php
@@ -467,7 +467,7 @@ try {
   <div class="offcanvas-body sidebar">
     <div class="side-brand mb-2"><h3><span>Committee</span></h3></div>
     <nav class="sidebar-menu">
-      <a href="committee_dashboard.php"><i class="fa-solid fa-border-all"></i>ภาพรวม</a>
+      <a href="committee_dashboard.php"><i class="fa-solid fa-border-all"></i>แดชบอร์ด</a>
       <a href="finance.php" class="active"><i class="fa-solid fa-wallet"></i> การเงินและบัญชี</a>
       <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
       <a href="member.php"><i class="bi bi-people-fill"></i> สมาชิก</a>
@@ -482,7 +482,7 @@ try {
     <aside class="col-lg-2 d-none d-lg-flex flex-column sidebar py-4">
       <div class="side-brand mb-3"><h3><span>Committee</span></h3></div>
       <nav class="sidebar-menu flex-grow-1">
-        <a href="committee_dashboard.php"><i class="fa-solid fa-border-all"></i>ภาพรวม</a>
+        <a href="committee_dashboard.php"><i class="fa-solid fa-border-all"></i>แดชบอร์ด</a>
         <a href="finance.php" class="active"><i class="fa-solid fa-wallet"></i> การเงินและบัญชี</a>
         <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
         <a href="member.php"><i class="bi bi-people-fill"></i> สมาชิก</a>
