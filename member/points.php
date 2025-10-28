d85331e72b7c1f4755bd077673300e01a4671834
treepalm3 <palmmy.tree13@gmail.com>
2025-10-28 15:46:55 +0700

    แก้แดชบอร์ด

--- member/points.php
+++ member/points.php
@@ -264,7 +264,7 @@ if ($db_ok) {
     <div class="offcanvas-body sidebar">
       <div class="side-brand mb-2"><h3><span>Member</span></h3></div>
       <nav class="sidebar-menu">
-        <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>ภาพรวม</a>
+        <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>แดชบอร์ด</a>
         <a href="bills.php"><i class="fa-solid fa-receipt"></i> ประวัติการซื้อ</a>
         <a class="active" href="points.php"><i class="fa-solid fa-star"></i> คะแนนสะสม</a>
         <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
@@ -279,7 +279,7 @@ if ($db_ok) {
       <aside class="col-lg-2 d-none d-lg-flex flex-column sidebar py-4">
         <div class="side-brand mb-3"><h3><span>Member</span></h3></div>
         <nav class="sidebar-menu flex-grow-1">
-          <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>ภาพรวม</a>
+          <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>แดชบอร์ด</a>
           <a href="bills.php"><i class="fa-solid fa-receipt"></i> ประวัติการซื้อ</a>
           <a class="active" href="points.php"><i class="fa-solid fa-star"></i> คะแนนสะสม</a>
           <a href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล</a>
