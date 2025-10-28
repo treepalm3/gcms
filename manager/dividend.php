b62484ea09adbe485bafb1a27da5abd370e0d64f
treepalm3 <palmmy.tree13@gmail.com>
2025-10-28 07:48:13 +0700

    แก้แดชบอร์ด

--- manager/dividend.php
+++ manager/dividend.php
@@ -331,7 +331,7 @@ try {
   <div class="offcanvas-body sidebar">
     <div class="side-brand mb-2"><h3><span>Manager</span></h3></div>
     <nav class="sidebar-menu">
-      <a href="manager_dashboard.php"><i class="fa-solid fa-border-all"></i>ภาพรวม</a>
+      <a href="manager_dashboard.php"><i class="fa-solid fa-border-all"></i>แดชบอร์ด</a>
       <a href="inventory.php"><i class="bi bi-fuel-pump-fill"></i>จัดการน้ำมัน</a>
       <a href="committee.php"><i class="fas fa-users-cog"></i> กรรมการ</a>
       <a href="employee.php"><i class="bi bi-person-badge-fill"></i>พนักงาน</a>
@@ -350,7 +350,7 @@ try {
     <aside class="col-lg-2 d-none d-lg-flex flex-column sidebar py-4">
       <div class="side-brand mb-3"><h3><span>Manager</span></h3></div>
       <nav class="sidebar-menu flex-grow-1">
-        <a href="manager_dashboard.php"><i class="fa-solid fa-border-all"></i>ภาพรวม</a>
+        <a href="manager_dashboard.php"><i class="fa-solid fa-border-all"></i>แดชบอร์ด</a>
         <a href="inventory.php"><i class="bi bi-fuel-pump-fill"></i>จัดการน้ำมัน</a>
         <a href="committee.php"><i class="fas fa-users-cog"></i> กรรมการ</a>
         <a href="employee.php"><i class="bi bi-person-badge-fill"></i>พนักงาน</a>
