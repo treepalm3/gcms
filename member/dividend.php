fe5fa7f0b6e0334ddca2b65521882e78807fb099
treepalm3 <palmmy.tree13@gmail.com>
2025-10-28 15:46:35 +0700

    แก้แดชบอร์ด

--- member/dividend.php
+++ member/dividend.php
@@ -268,7 +268,7 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
     <div class="offcanvas-body sidebar">
       <div class="side-brand mb-2"><h3><span>Member</span></h3></div>
       <nav class="sidebar-menu">
-        <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>ภาพรวม</a>
+        <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>แดชบอร์ด</a>
         <a href="bills.php"><i class="fa-solid fa-receipt"></i> ประวัติการซื้อ</a>
         <a href="points.php"><i class="fa-solid fa-star"></i> คะแนนสะสม</a>
         <a class="active" href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล & เฉลี่ยคืน</a>
@@ -283,7 +283,7 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
       <aside class="col-lg-2 d-none d-lg-flex flex-column sidebar py-4">
         <div class="side-brand mb-3"><h3><span>Member</span></h3></div>
         <nav class="sidebar-menu flex-grow-1">
-          <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>ภาพรวม</a>
+          <a href="member_dashboard.php"><i class="fa-solid fa-id-card"></i>แดชบอร์ด</a>
           <a href="bills.php"><i class="fa-solid fa-receipt"></i> ประวัติการซื้อ</a>
           <a href="points.php"><i class="fa-solid fa-star"></i> คะแนนสะสม</a>
           <a class="active" href="dividend.php"><i class="fa-solid fa-gift"></i> ปันผล & เฉลี่ยคืน</a>
